32415bff61a73054e00535f07c5bb4b01f11dcaa
Hayesgavin-commits <Hayes_gavin@rocketmail.com>
2026-09-01 15:30:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gavvas-astro-v1';
+const CACHE='gavvas-astro-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
